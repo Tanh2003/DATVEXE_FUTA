0d69d9e9803c02fc0ff9f2c9025100291c9f32a3
thanh <lmthanh203@gmail.com>
2023-11-22 14:17:45 +0700

    fix giao diện datve

--- datvexefuta/src/User/DatXe.js
+++ datvexefuta/src/User/DatXe.js
@@ -79,121 +79,134 @@ function Booking() {
   }
   return (
     <main>
-    <div className="dsmain">
-      <div className="custom-box table-container2">
-        {/* Bảng thứ nhất */}
+      <div className="dsmain">
+        <div className="custom-box table-container2">
+          {/* Bảng thứ nhất */}
+          <div className="table-container2">
+            <div className="titleDV">
+              <p> Chọn ghế để đặt vé </p>
+            </div>
+            <div className="row tt2">
+              <div className="col">
+                <p> Tầng dưới </p>
+              </div>
+              <div className="col">
+                <p> Tầng trên </p>
+              </div>
+            </div>
+            <div className="custom-dv">
+              <div className="row ds">
+                {Array(numRows)
+                  .fill()
+                  .map((_, rowIndex) => (
+                    <div className="seats col" key={rowIndex}>
+                      {Array(numSeatsPerRow)
+                        .fill()
+                        .map((_, index) => {
+                          const seatIndex = calculateSeatIndex(rowIndex, index);
+                          const isOccupied = seats[seatIndex];
+                          const seatNumber = seatIndex + 1;
+                          const seatName = getSeatName(seatNumber);
+                          return (
+                            <div
+                              key={seatIndex}
+                              className={`seat ${
+                                isOccupied ? "occupied" : "available"
+                              }`}
+                              onClick={() => handleSeatClick(rowIndex, index)}
+                            >
+                              {isOccupied ? (
+                                <div className="seat-content">
+                                  <img
+                                    src="/image/ghe2.jpeg"
+                                    alt="Occupied Seat"
+                                    className="ds-img"
+                                  />
+                                </div>
+                              ) : (
+                                <div className="seat-content">
+                                  <span className="seat-number">
+                                    {seatName}
+                                  </span>
+                                  <img
+                                    src="/image/ghe1.png"
+                                    alt="Available Seat"
+                                    className="ds-img"
+                                  />
+                                </div>
+                              )}
+                            </div>
+                          );
+                        })}
+                    </div>
+                  ))}
+              </div>
+              <div className="dsghe">
+                <p> Ghế chưa chọn </p> <br />
+                <p> Ghế đang chọn </p> <br />
+                <p> Đã bán </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Bảng thứ hai */}
+          <div>
+            <div className="pickup-dropoff">
+              <div className="pickup">
+                <label> Điểm đón: </label>
+                <select onChange={handleDropoffChange} value={pickupLocation}>
+                  <option value="Phú Yên"> Phú Yên </option>
+                </select>
+              </div>
+              <div className="dropoff">
+                <label> Điểm trả khách: </label>
+                <select onChange={handleDropoffChange} value={dropOffLocation}>
+                  <option value="Bến Xe Miền Đông"> Bến Xe Miền Đông </option>
+                  <option value="Bến Xe Miền Tây"> Bến Xe Miền Tây </option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="table-container2">
-          <div className="titleDV">
-            <p> Chọn ghế để đặt vé </p>
+          <div className="selected-seats dstable">
+            <p> Danh sách ghế đã chọn: {selectedSeats.join(", ")} </p>
+            <p> Tổng số ghế đã chọn: {selectedSeats.length} </p>
+            <p> Tổng tiền: {formatPrice(selectedSeats.length * 255000)} </p>
+            <p>
+              {pickupLocation} <i className="fas fa-angle-double-right"> </i>
+              {dropOffLocation}
+            </p>
           </div>
-          <div className="row tt2">
-            <div className="col">
-              <p> Tầng dưới </p>
-            </div>
-            <div className="col">
-              <p> Tầng trên </p>
-            </div>
-          </div>
-          <div className="custom-dv">
-            <div className="row ds">
-              {Array(numRows)
-                .fill()
-                .map((_, rowIndex) => (
-                  <div className="seats col" key={rowIndex}>
-                    {Array(numSeatsPerRow)
-                      .fill()
-                      .map((_, index) => {
-                        const seatIndex = calculateSeatIndex(rowIndex, index);
-                        const isOccupied = seats[seatIndex];
-                        const seatNumber = seatIndex + 1;
-                        const seatName = getSeatName(seatNumber);
-                        return (
-                          <div
-                            key={seatIndex}
-                            className={`seat ${
-                              isOccupied ? "occupied" : "available"
-                            }`}
-                            onClick={() => handleSeatClick(rowIndex, index)}
-                          >
-                            {isOccupied ? (
-                              <i class="fas fa-couch"> </i>
-                            ) : (
-                              seatName
-                            )}
-                          </div>
-                        );
-                      })}
-                  </div>
-                ))}
-            </div>
-            <div className="dsghe">
-              <p> Ghế chưa chọn </p> <br />
-              <p> Ghế đang chọn </p> <br />
-              <p> Đã bán </p>
-            </div>
-          </div>
-        </div>
-
-        {/* Bảng thứ hai */}
-        <div>
-          <div className="pickup-dropoff">
-            <div className="pickup">
-              <label> Điểm đón: </label>
-              <select onChange={handleDropoffChange} value={pickupLocation}>
-                <option value="Phú Yên"> Phú Yên </option>
-              </select>
-            </div>
-            <div className="dropoff">
-              <label> Điểm trả khách: </label>
-              <select onChange={handleDropoffChange} value={dropOffLocation}>
-                <option value="Bến Xe Miền Đông"> Bến Xe Miền Đông </option>
-                <option value="Bến Xe Miền Tây"> Bến Xe Miền Tây </option>
-              </select>
+          <div className="custom-box3 table-container2">
+            <div className="">
+              <p className="title3">Thông tin khách hàng</p>
+              <div className="row">
+                <input
+                  type="text3"
+                  className="custom-inp"
+                  name="user_name"
+                  placeholder="Họ và tên"
+                />
+              </div>
+              <div className="row">
+                <input
+                  type="text3"
+                  name="user_phonenumber"
+                  placeholder="Số điện thoại"
+                />
+              </div>
+              <div className="row">
+                <input type="text3" name="user_email" placeholder="Email" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="table-container2">
-        <div className="selected-seats dstable">
-          <p> Danh sách ghế đã chọn: {selectedSeats.join(", ")} </p>
-          <p> Tổng số ghế đã chọn: {selectedSeats.length} </p>
-          <p> Tổng tiền: {formatPrice(selectedSeats.length * 255000)} </p>
-          <p>
-            {pickupLocation} <i className="fas fa-angle-double-right"> </i>
-            {dropOffLocation}
-          </p>
-        </div>
-        <div className="custom-box3 table-container2">
-        
-          <div className=""><p className="title3">Thông tin khách hàng</p>
-            <div className="row">
-              <input
-                type="text3"
-                className="custom-inp"
-                name="user_name"
-                placeholder="Họ và tên"
-              />
-            </div>
-            <div className="row">
-              <input
-                type="text3"
-                name="user_phonenumber"
-                placeholder="Số điện thoại"
-              />
-            </div>
-            <div className="row">
-              <input type="text3" name="user_email" placeholder="Email" />
-            </div>
-          </div>
-        </div>
-      </div>
-      
-    </div>
-    <div className="col">
+      <div className="col">
         <button className="custom-btn2"> Đặt vé </button>
-    </div>
+      </div>
     </main>
-
   );
 }
 
